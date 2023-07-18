41cfe5a65aa8a698df143909f4ef9d2554b17f62
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-18 11:04:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json()
+      if (!response.ok) {
+        throw new Error('Something went wrong.')
+      }
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error.message)
+    }
+
+  }
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
